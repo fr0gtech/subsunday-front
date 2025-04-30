77501e43a9c2c31e1c03ce26a23e4b84487e2254
GREEB <gr44bli@gmail.com>
2025-04-30 18:49:32 +0200

    fix: style current cotes

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,8 @@ export default function Home() {
 
   return (
     <section className="flex h-full flex-col items-start justify-start gap-4">
-      <CurrentVotes className="flex lg:hidden p-5 gap-3 text-tiny" />
+      <CurrentVotes className="flex lg:hidden mx-auto p-2 gap-3 text-tiny" />
       <MainItem />
-
     </section>
   );
 }
