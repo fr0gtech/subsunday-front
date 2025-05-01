ffd522b220eca9b8b047ae76b74cdac2556c87c5
GREEB <gr44bli@gmail.com>
2025-05-01 21:23:12 +0200

    fix: center user card

--- app/user/[slug]/page.tsx
+++ app/user/[slug]/page.tsx
@@ -3,7 +3,7 @@ import { UserComp } from '@/components/userComp';
 export default async function Home({ params }: { params: Promise<{ slug: string }> }) {
   const { slug } = await params;
   return (
-    <section className="flex h-full flex-col items-start justify-start gap-4">
+    <section className="flex w-full h-full flex-col items-start justify-start gap-4">
       <UserComp id={slug} />
     </section>
   );
