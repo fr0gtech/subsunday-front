38d215eb359c9c08ba819f3361399da674d470b1
GREEB <gr44bli@gmail.com>
2025-05-01 21:37:54 +0200

    fix: maybe relative times now update every 5 secs?

--- components/voted.tsx
+++ components/voted.tsx
@@ -2,17 +2,29 @@ import { Vote } from '@/generated/prisma';
 import { Card, CardBody, Chip } from '@heroui/react';
 import { formatDistance } from 'date-fns';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 export const Voted = ({
   vote,
 }: {
   vote: Vote & { from: { name: string; id: number } } & { for: { id: number; name: string } };
 }) => {
+  const [time, setTime] = useState<Date>(new Date())
+  // make relative dates update, this is probably not the best way to do this but should be fine if we only display a few items
+  useEffect(() =>{
+    const interval = setInterval(()=>{
+      setTime(new Date())
+    }, 5000)
+
+    return () =>{
+      clearInterval(interval)
+    }
+  },[])
   return (
     <Card shadow='md'>
       <CardBody className="">
         <span className="text-tiny !text-left leading-8">
-          {formatDistance(new Date(vote.createdAt), new Date(), { addSuffix: true })}{' '}
+          {time && formatDistance(new Date(vote.createdAt), new Date(), { addSuffix: true })}{' '}
           <Link href={`/user/${vote.from.id}`}>
             <Chip size="sm" color="primary" variant="flat" className="whitespace-pre-wrap">
               {vote.from.name}
