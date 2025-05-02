f6dbd5308a45ff72b70a343030e801f1d8ca7032
GREEB <gr44bli@gmail.com>
2025-05-02 22:50:32 +0200

    fix: new grid kinda works should support more screensizes and look ok

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { MainItem } from '@/components/mainItem';
 export default function Home() {
 
   return (
-    <section className="flex h-full flex-col items-start justify-start gap-4">
+    <section className="flex h-full flex-col items-start justify-start gap-4 w-full">
       <CurrentVotes className="flex lg:hidden mx-auto p-2 gap-3 text-tiny" />
       <MainItem />
     </section>
--- components/mainItem.tsx
+++ components/mainItem.tsx
@@ -4,7 +4,7 @@ import clsx from "clsx";
 import Link from "next/link";
 import { Logo, Steamicon } from "./icons";
 import { socket, fetcher } from "@/app/lib";
-import { useState, useEffect, useMemo, useCallback } from "react";
+import { useState, useEffect, useMemo, useCallback, useRef } from "react";
 import useSWR from "swr";
 import { wsMsg } from "@/types";
 import { LiveVotes } from "./liveVotes";
@@ -15,6 +15,7 @@ export type gameNcount = Game & {
 }
 export const MainItem = () => {
     const [msgEvents, setMsgEvents] = useState<wsMsg[]>([]);
+
 
     useEffect(() => {
         const onMsgEvent = (value: wsMsg) => {
@@ -59,6 +60,7 @@ export const MainItem = () => {
             .sort((a: any, b: any) => (a._count.votes > b._count.votes ? -1 : 1));
     }, [msgEvents, data]);
 
+
     if (isLoading) {
         return (
             <section className="flex h-full flex-col items-start justify-start gap-4">
@@ -90,10 +92,10 @@ export const MainItem = () => {
         )
     }
     return (
-        <div className="flex p-3">
-            <div className=" lg:p-5 flex flex-wrap gap-7">
+        <div className="flex p-3 w-full">
+            <div className="container2 w-full">
                 {updateableGames &&
-                    updateableGames.slice(0, 3).map(
+                    updateableGames.map(
                         (e, i: number,
                         ) => {
                             return (
@@ -101,21 +103,14 @@ export const MainItem = () => {
                             );
                         },
                     )}
-                <div className="hidden lg:block">
-
-                <LiveVotes amount={3} bg={false} textRight />
+                <div className="fixed2 relative w-full h-full overflow-hidden">
+                    <div className="absolute w-full h-full top-0 left-0 whitespace-nowrap overflow-hidden2">
+                        <LiveVotes amount={3} bg={false} textRight />
+                    </div>
                 </div>
-                <Divider className="hidden lg:visible"/>
-                {updateableGames &&
-                    updateableGames.slice(3).map(
-                        (e, i: number,
-                        ) => {
-                            return (
-                                <MainCard key={e.id} e={e} i={i + 3} />
-                            );
-                        },
-                    )}
             </div>
+            <Divider className="hidden lg:visible" />
+
         </div>
 
     )
