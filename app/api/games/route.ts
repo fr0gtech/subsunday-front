5e17894e5af5ff77dba7e98e1d8260f9bbacc18d
GREEB <gr44bli@gmail.com>
2025-05-02 21:38:37 +0200

    fix: load a bunch of items cuz i don't want to do pagination

--- app/api/games/route.ts
+++ app/api/games/route.ts
@@ -2,6 +2,14 @@ import { NextResponse } from 'next/server';
 import { prisma } from '@/prisma';
 
 export async function GET() {
+
+  // we need to cursor this req and depending on screen size we
+  // load more games on useIntersec? (visible) if needed this is the best way to create the exact amount of items we need?
+  // but how would mutate work? we don't need cuz of socket?
+  // calcing by screensize sounds dumb
+  // easiest way to do it would be to provide index but still mutate?
+  //
+  // or we just load a shitton and don't give a fuck? <-
   const games = await prisma.game.findMany({
     where: {
       name: {
@@ -18,7 +26,7 @@ export async function GET() {
         select: { votes: true },
       },
     },
-    take: 15,
+    take: 50,
   });
 
   return NextResponse.json({
