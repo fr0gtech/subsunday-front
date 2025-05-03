4e25465bfcc10f99f6c486037c4ad171fe18a5b6
GREEB <gr44bli@gmail.com>
2025-05-03 13:33:17 +0200

    fix: period should rerender all 5 secs

--- components/votingPeriod.tsx
+++ components/votingPeriod.tsx
@@ -1,9 +1,12 @@
 import { getDateRange } from "@/app/lib"
 import { TZDate } from "@date-fns/tz";
 import { formatDistance, isAfter } from "date-fns"
-import { useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 export const VotingPeriod = ({ className }: { className: string }) => {
+    
+  const [time, setTime] = useState<Date>(new Date())
+
     // display period of voting on a calendar or something?
     const voteRange = getDateRange({ fromDay: 1, fromTime: "00:00", toDay: 6, toTime: "22:00" })
 
@@ -12,13 +15,23 @@ export const VotingPeriod = ({ className }: { className: string }) => {
         return isAfter(today, voteRange.endDate)
     },[])
 
+      // make relative dates update, this is probably not the best way to do this but should be fine if we only display a few items
+      useEffect(() =>{
+        const interval = setInterval(()=>{
+          setTime(new Date())
+        }, 5000)
+    
+        return () =>{
+          clearInterval(interval)
+        }
+      },[])
     return (
         <p className={className}>
             {votingClosed &&
-                `voting start in: ${formatDistance(new Date(), voteRange.startDate)}`
+                `voting start in: ${time && formatDistance(new Date(), voteRange.startDate)}`
             }
             {!votingClosed &&
-                `vote ends in: ${formatDistance(new Date(), voteRange.endDate)}`
+                `vote ends in: ${time && formatDistance(new Date(), voteRange.endDate)}`
             }
         </p>
     )
