0d4e30ab48ca2671ab45f9b90f9c85485627d973
GREEB <gr44bli@gmail.com>
2025-05-04 21:54:46 +0200

    fix: loading skeleton

--- components/mainItem.tsx
+++ components/mainItem.tsx
@@ -82,7 +82,72 @@ export const MainItem = () => {
   }, [msgEvents, data]);
 
   if (!data || !data.games) {
-    return <div>loading</div>
+    return (
+      <div className="flex w-full justify-center items-center">
+        <div className="flex w-full p-4">
+          <div className="grid-container">
+            {isLoading &&
+              [...Array(25).fill(0)].map((e, i: number) => {
+                return (
+                  <Card
+                    key={i}
+                    className="overflow-visible w-full min-w-[332px] grow min-h-[186px] cursor-pointer"
+                  >
+                    <div className="relative flex flex-col h-full">
+                      <Skeleton
+                        isLoaded={data && data.games.length === 0}
+                        className={clsx([
+                          'flex flex-col justify-center grow min-h-[100px] min-w-[294px]  items-center z-0 w-full object-cover scale-[1.02] shadow-lg  border-4 rounded-[1em]',
+                          'dark:border-default light:border-neutral-200',
+                        ])}
+                      />
+                      <div className="flex p-1">
+                        <div className="flex flex-grow gap-2">
+                          <div className="flex gap-2 p-2 max-w-[300px]">
+                            <Skeleton
+                              isLoaded={data && data.games.length === 0}
+                              className="font-bold text-left whitespace-pre-wrap rounded-lg !w-20 !h-6">
+
+                            </Skeleton>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </Card>
+                );
+              })}
+            {/* <div className='bg-black absolute z-[9999999] top-0 left-0'>
+            <pre>
+              {JSON.stringify(selectedRange, null, 2)}
+            </pre>
+          </div> */}
+            <div className="fixed2 relative w-full h-full overflow-hidden">
+
+              <div className="absolute w-full h-full top-0 left-0 whitespace-nowrap overflow-hidden2">
+                <LiveVotes amount={3} bg={false} />
+              </div>
+            </div>
+            <div className="fixed3 flex flex-col justify-evenly p-3  ">
+              <VotingPeriod className="text-xl w-full text-center" />
+              <CurrentVotes className="gap-5 justify-center flex flex-row text-tiny" />
+              <div className="mt-5 opacity-70 block lg:hidden">
+                <div className=" text-center w-full text-xs flex flex-row justify-center mt-5 gap-2 items-center !leading">
+                  *
+                  <div>
+                    {' '}
+                    this is <b>not</b> an official sub sunday website
+                  </div>{' '}
+                  <Link href={'info'}>
+                    <InfoCircledIcon />
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+          <Divider className="hidden lg:visible" />
+        </div>
+      </div>
+    )
   }
   return (
     <div className="flex w-full justify-center items-center">
