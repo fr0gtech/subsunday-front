3d8e4d677b4d0a3dae51f196766e348ea6bc073c
GREEB <gr44bli@gmail.com>
2025-05-12 23:08:51 +0200

    fix: blurr

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { MainItem } from '@/components/mainItem';
 
 export default function Home() {
   return (
-    <section className="relative flex h-full flex-col items-start justify-start gap-4 w-full mt-16">
+    <section className="relative flex h-full flex-col items-start justify-start gap-4 w-full pt-16 ">
       {/* <CurrentVotes className="flex lg:hidden mx-auto p-2 gap-3 text-tiny" /> */}
       <MainItem />
     </section>
