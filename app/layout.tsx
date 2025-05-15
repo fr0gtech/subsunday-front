fda27f73a4d26874e595a3f2767efe66e94ed4cb
GREEB <gr44bli@gmail.com>
2025-05-15 15:44:43 +0200

    fix: add matomo

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,11 +34,19 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html suppressHydrationWarning lang="en">
       <head>
         <meta name="apple-mobile-web-app-title" content="sub-sunday" />
-        <script
-          src="https://app.rybbit.io/api/script.js"
-          data-site-id="551"
-          defer
-        ></script>
+        <script>
+          var _paq = window._paq = window._paq || [];
+          /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+          _paq.push(['trackPageView']);
+          _paq.push(['enableLinkTracking']);
+          (function() {
+            var u="//matomo.hinny-corn.ts.net/";
+          _paq.push(['setTrackerUrl', u+'matomo.php']);
+          _paq.push(['setSiteId', '1']);
+          var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+          g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+          })();
+        </script>
       </head>
       <body
         className={clsx(
