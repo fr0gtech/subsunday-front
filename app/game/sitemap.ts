bd285c3eaceea7dd1ea61f3f8a53f9716bee7f2c
GREEB <gr44bli@gmail.com>
2025-05-20 20:30:25 +0200

    fix: sitemap of games

--- app/game/sitemap.ts
+++ app/game/sitemap.ts
@@ -2,11 +2,11 @@ import { prisma } from '@/prisma';
 
 export default async function sitemap() {
   const products = await prisma.game.findMany({
-    select: { id: true, updatedAt: true },
+    select: { id: true, updatedAt: true, steamId: true },
   });
 
   return products.map((product) => ({
-    url: `${process.env.NEXT_PUBLIC_BASE_URL}/game/${product.id}`,
+    url: `${process.env.NEXT_PUBLIC_BASE_URL}/game/${product.steamId || product.id}`,
     lastModified: product.updatedAt,
   }));
 }
